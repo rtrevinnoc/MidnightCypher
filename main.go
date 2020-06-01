cd1cf34b8c4d164554a2d44fd8606bfaacfe7721
rtrevinnoc <rtrevinnoc@hotmail.com>
2020-06-01 02:42:55 -0500

    Testing image writing in heroku

--- main.go
+++ main.go
@@ -1,16 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"encoding/json"
+	"io/ioutil"
+	"io"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 
 	"math/rand"
 	"time"
+	"strings"
 )
+
+type Urls struct {
+	Body string
+	Domain string
+	Header string
+	Language string
+	Url string
+}
+
+type FUTUREResponse struct {
+	Answer string
+	Chatbot int
+	Corrected string
+	Images []string
+	Map string
+	NRes int
+	Reply string
+	SmallSummary string
+	Time float64
+	Urls []Urls
+}
+
+type FUTUREResult struct {
+	Result FUTUREResponse
+}
 
 func main() {
 
@@ -128,7 +158,40 @@ func main() {
 	})
 
 	router.POST("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"response": c.PostForm("secretMessage"), "rand": left[rand.Intn(93)]})
+		imageUrlComponents := []string{"http://192.168.1.73/_answer?query=", left[rand.Intn(93)]}
+		imageUrl := strings.Join(imageUrlComponents, "")
+
+    		response, err := http.Get(imageUrl)
+    		if err != nil {
+        		log.Fatal(err)
+    		}
+    		defer response.Body.Close()
+ 
+    		responseData, err := ioutil.ReadAll(response.Body)
+    		if err != nil {
+        		log.Fatal(err)
+    		}
+		
+		responseString := string(responseData)
+		fmt.Printf(responseString)
+ 
+		futureResult := new(FUTUREResult)
+		json.Unmarshal([]byte(responseData), futureResult)
+		fmt.Println(futureResult)
+		listOfImages := futureResult.Result.Images
+		numberOfImages := len(listOfImages) 
+		futureImageUrl := listOfImages[rand.Intn(numberOfImages)]
+
+		img, _ := os.Create("image.jpg")
+    		defer img.Close()
+
+    		resp, _ := http.Get(futureImageUrl)
+    		defer resp.Body.Close()
+
+    		b, _ := io.Copy(img, resp.Body)
+    		fmt.Println("File size: ", b)	
+
+		c.JSON(http.StatusOK, gin.H{"response": c.PostForm("secretMessage"), "rand": futureImageUrl})
 	})
 
 	router.Static("/.well-known/pki-validation", "2FF0A0CC6029BB26BB49BEDD95CE23F8.txt")
