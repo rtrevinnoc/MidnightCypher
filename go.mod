03082ef485d3a4a38a101c0472db23d325f15d9e
rtrevinnoc <rtrevinnoc@hotmail.com>
2020-06-01 00:06:34 -0500

    Minimal views and SSL certificate

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/mattn/go-colorable v0.0.0-20150625154642-40e4aedc8fab // indirect
 	github.com/mattn/go-isatty v0.0.0-20150814002629-7fcbc72f853b // indirect
 	github.com/stretchr/testify v1.3.0 // indirect
-	golang.org/x/net v0.0.0-20150629084131-d9558e5c97f8 // indirect
+	github.com/zhexuany/wordGenerator v0.0.0-20161102120352-1f13e790d534
 	gopkg.in/bluesuncorp/validator.v5 v5.9.1 // indirect
+	lukechampine.com/jsteg v1.0.0 // indirect
 )
--- main.go
+++ main.go
@@ -7,9 +7,111 @@ import (
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
+
+	"math/rand"
+	"time"
 )
 
 func main() {
+
+	left := [...]string{
+		"admiring",
+		"adoring",
+		"affectionate",
+		"agitated",
+		"amazing",
+		"angry",
+		"awesome",
+		"blissful",
+		"boring",
+		"brave",
+		"clever",
+		"cocky",
+		"compassionate",
+		"competent",
+		"condescending",
+		"confident",
+		"cranky",
+		"dazzling",
+		"determined",
+		"distracted",
+		"dreamy",
+		"eager",
+		"ecstatic",
+		"elastic",
+		"elated",
+		"elegant",
+		"eloquent",
+		"epic",
+		"fervent",
+		"festive",
+		"flamboyant",
+		"focused",
+		"friendly",
+		"frosty",
+		"gallant",
+		"gifted",
+		"goofy",
+		"gracious",
+		"happy",
+		"hardcore",
+		"heuristic",
+		"hopeful",
+		"hungry",
+		"infallible",
+		"inspiring",
+		"jolly",
+		"jovial",
+		"keen",
+		"kind",
+		"laughing",
+		"loving",
+		"lucid",
+		"mystifying",
+		"modest",
+		"musing",
+		"naughty",
+		"nervous",
+		"nifty",
+		"nostalgic",
+		"objective",
+		"optimistic",
+		"peaceful",
+		"pedantic",
+		"pensive",
+		"practical",
+		"priceless",
+		"quirky",
+		"quizzical",
+		"relaxed",
+		"reverent",
+		"romantic",
+		"sad",
+		"serene",
+		"sharp",
+		"silly",
+		"sleepy",
+		"stoic",
+		"stupefied",
+		"suspicious",
+		"tender",
+		"thirsty",
+		"trusting",
+		"unruffled",
+		"upbeat",
+		"vibrant",
+		"vigilant",
+		"vigorous",
+		"wizardly",
+		"wonderful",
+		"xenodochial",
+		"youthful",
+		"zealous",
+		"zen",
+	}
+
+	rand.Seed(time.Now().UnixNano())
+
 	port := os.Getenv("PORT")
 
 	if port == "" {
@@ -24,6 +126,12 @@ func main() {
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
+
+	router.POST("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"response": c.PostForm("secretMessage"), "rand": left[rand.Intn(93)]})
+	})
+
+	router.Static("/.well-known/pki-validation", "static/2FF0A0CC6029BB26BB49BEDD95CE23F8.txt")
 
 	router.Run(":" + port)
 }
